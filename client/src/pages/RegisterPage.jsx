97935813d8c01630b082858048a0c3a19cf76041
homoki-denis <homoki.denisth@gmail.com>
2023-10-16 11:35:04 +0300

    feat: configurating api and register page

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
+
 
 export default function RegisterPage() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    function registerUser(ev) {
+        ev.preventDefault();
+        axios.post('/register', {
+            name, 
+            email,
+            password
+        });
+    }
+
+
+
     return (
         <div className="mt-4 grow flex items-center justify-center">
             <div className="-mt-8">
             <h1 className="text-4xl text-center mb-4">Register</h1>
-            <form className="max-w-md mx-auto" action="">
-                <input type="text" placeholder="your name" name="" id="" />
-                <input type="email" placeholder="your@email.com" name="" id="" />
-                <input type="password" placeholder="password"  />
+            <form className="max-w-md mx-auto" onSubmit={registerUser}>
+                <input type="text" placeholder="your name" value={name} onChange={ev => setName(ev.target.value)}/>
+                <input type="email" placeholder="your@email.com"  value={email} onChange={ev => setEmail(ev.target.value)}/>
+                <input type="password" placeholder="password"  value={password} onChange={ev => setPassword(ev.target.value)}/>
                 <button className="primary">Register</button>
                     <div className="py-1">Already a member? <Link className="underline text-black" to={'/login'}>Log in</Link>
                     </div>
