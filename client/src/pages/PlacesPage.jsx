cf5bd3d939666708e97e154e587f1d7c9e0245a8
homoki-denis <homoki.denisth@gmail.com>
2023-10-23 12:03:15 +0300

    feat: places page

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,31 +1,69 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 export default function PlacesPage() {
+  const { action } = useParams();
+
   return (
     <div>
-      <div className="text-center">
-        <Link
-          className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full"
-          to={'/account/places/new'}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
+      {action !== 'new' && (
+        <div className="text-center">
+          <Link
+            className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full"
+            to={'/account/places/new'}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 4.5v15m7.5-7.5h-15"
-            />
-          </svg>
-          Add new place
-        </Link>
-      </div>
-      my places
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+            Add new place
+          </Link>
+        </div>
+      )}
+      {action === 'new' && (
+        <div>
+          <form>
+            <h2 className="text-xl mt-4">Title</h2>
+            <input type="text" placeholder="Title, ex: My appartment" />
+            <h2 className="text-xl mt-4">Address</h2>
+            <input type="text" placeholder="Address" />
+            <h2 className="text-xl mt-4">Photos</h2>
+            <div className="flex gap-2">
+              <input type="text" placeholder="Add with a link" />
+              <button className="bg-gray-200 px-4 rounded-2xl">
+                Add&nbsp;Photo
+              </button>
+            </div>
+            <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 mt-2">
+              <button className="flex justify-center border bg-transparent rounded-2xl p-16 text-2xl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-8 h-8"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+                  />
+                </svg>
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
