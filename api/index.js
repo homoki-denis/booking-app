1871d16928fe09aa9595c65eb0d3931996c68212
homoki-denis <homoki.denisth@gmail.com>
2024-01-17 00:08:34 +0200

    feat: added upload by link

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const { default: mongoose } = require("mongoose");
 const jwt = require("jsonwebtoken");
 const cookieParse = require("cookie-parser");
+const imageDownloader = require("image-downloader");
+const path = require("path");
 
 require("dotenv").config();
 
@@ -82,6 +84,19 @@ app.get("/profile", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.cookie("token", "").json(true);
+});
+
+app.post("/upload-by-link", async (req, res) => {
+  const { link } = req.body;
+
+  const newName = "photo" + Date.now() + ".jpg";
+
+  await imageDownloader.image({
+    url: link,
+    dest: __dirname + "/uploads/" + newName,
+  });
+
+  res.json(newName);
 });
 
 app.listen(4000);
---
+++ readme.md
@@ -0,0 +1,1 @@
+NluDd1n1N49KcUpw password
